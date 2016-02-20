ce41c0bdb64b487d15489effd36577324e6a7aa8
Ovidiu Caba <ovidiu.caba@gmail.com>
2016-02-20 08:58:22 +0200

    Add BO project and County model

---
+++ src/StatisticsRomania.BusinessObjects/County.cs
@@ -0,0 +1,17 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StatisticsRomania.BusinessObjects
+{
+    public class County
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(8)]
+        public string Name { get; set; }
+    }
+}
