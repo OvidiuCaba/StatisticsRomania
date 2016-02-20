2d325932fefcbf4fd11a024f37a78985293a525a
Ovidiu Caba <ovidiu.caba@gmail.com>
2016-02-20 10:24:34 +0200

    Add the root page - a tabbed view with County details and County standings

--- src/StatisticsRomania/StatisticsRomania/App.cs
+++ src/StatisticsRomania/StatisticsRomania/App.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using StatisticsRomania.Views;
 using Xamarin.Forms;
 
 namespace StatisticsRomania
@@ -12,19 +12,7 @@ namespace StatisticsRomania
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-                }
-            };
+            MainPage = new RootPage();
         }
 
         protected override void OnStart()
---
+++ src/StatisticsRomania/StatisticsRomania/Views/RootPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StatisticsRomania.Views
+{
+    public class RootPage : TabbedPage
+    {
+        public RootPage()
+        {
+            this.Title = "Romania - Statistici";
+            this.Children.Add(new ContentPage
+            {
+                Title = "Statistici judenete",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+						new Label {
+							XAlign = TextAlignment.Center,
+							Text = "We're going to display county details!"
+						}
+					}
+                }
+            });
+            this.Children.Add(new ContentPage
+            {
+                Title = "Clasamente",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+						new Label {
+							XAlign = TextAlignment.Center,
+							Text = "We're going to display county standings!"
+						}
+					}
+                }
+            });
+        }
+    }
+}
