2fd9705b81d2270e255d913e1213b65962e4fc53
Ovidiu Caba <ovidiu.caba@gmail.com>
2016-04-16 18:34:53 +0300

    Cleanup

--- src/StatisticsRomania.BusinessObjects/CountryIds.cs
+++ src/StatisticsRomania.BusinessObjects/CountryIds.cs
@@ -1,51 +1,9 @@
 ﻿using System.Collections.Generic;
+
 namespace StatisticsRomania.BusinessObjects
 {
     public static class CountryIds
     {
-        public static int Alba = 1;
-        public static int Arad = 2;
-        public static int Arges = 3;
-        public static int Bacau = 4;
-        public static int Bihor = 5;
-        public static int BistritaNasaud = 6;
-        public static int Botosani = 7;
-        public static int Brasov = 8;
-        public static int Braila = 9;
-        public static int Buzau = 10;
-        public static int CarasSeverin = 11;
-        public static int Calarasi = 12;
-        public static int Cluj = 13;
-        public static int Constanta = 14;
-        public static int Covasna = 15;
-        public static int Dambovita = 16;
-        public static int Dolj = 17;
-        public static int Galati = 18;
-        public static int Giurgiu = 19;
-        public static int Gorj = 20;
-        public static int Harghita = 21;
-        public static int Hunedoara = 22;
-        public static int Ialomita = 23;
-        public static int Iasi = 24;
-        public static int Ilfov = 25;
-        public static int Maramures = 26;
-        public static int Mehedinti = 27;
-        public static int Mures = 28;
-        public static int Neamt = 29;
-        public static int Olt = 30;
-        public static int Prahova = 31;
-        public static int SatuMare = 32;
-        public static int Salaj = 33;
-        public static int Sibiu = 34;
-        public static int Suceava = 35;
-        public static int Teleorman = 36;
-        public static int Timis = 37;
-        public static int Tulcea = 38;
-        public static int Vaslui = 39;
-        public static int Valcea = 40;
-        public static int Vrancea = 41;
-        public static int Bucuresti = 42;
-
         public static Dictionary<string, int> Counties = new Dictionary<string, int>()
                                                              {
                                                                  {"Alba", 1},
