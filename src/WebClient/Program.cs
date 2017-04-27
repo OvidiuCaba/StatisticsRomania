ede44d38787a54cf09bea416d57339062e2dd053
Ovidiu Caba <older_ovello@yahoo.com>
2017-04-27 23:29:20 +0300

    Add Application Insights

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -16,6 +16,7 @@ namespace WebClient
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
