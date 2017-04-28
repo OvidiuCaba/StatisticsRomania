5397c69e625118c3e9ddf8f36a3beffe7aa12a30
Ovidiu Caba <older_ovello@yahoo.com>
2017-04-28 23:37:32 +0300

    Add TODO

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
+// TODO: Ar fi interesant o statistica pe tot anul, mai ales cele care s-au incheiat. Nu doar pe luni. [de la Sabin Uivarosan]
+
 namespace WebClient
 {
     public class Program
