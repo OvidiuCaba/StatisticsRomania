335b9f893a9243e6daa53faceda25002fa28e62d
Ovidiu Caba <older_ovello@yahoo.com>
2017-04-29 09:21:17 +0300

    Add TODO

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
 // TODO: Ar fi interesant o statistica pe tot anul, mai ales cele care s-au incheiat. Nu doar pe luni. [de la Sabin Uivarosan]
+// TODO: Acum daca faci si totalul/media anuala cand apas la Clasamente pe an ar fi si mai bine ... sa compari situatii lunare e cam aiurea ... 
+//       as sugera cand dai click pe un an sa faca media/totalul pe an, iar cand apesi si pe o luna sa arate situatia pe luna respectiva. Evident la salarii sa faca media, la turisti sa faca totalul, etc. [utopium / SSC]
 
 namespace WebClient
 {
