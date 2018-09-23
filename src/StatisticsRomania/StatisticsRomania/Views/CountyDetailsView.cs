64188d0f4d57c5189dddc4de8b9e83ab63bc2cc4
Ovidiu Caba <older_ovello@yahoo.com>
2018-09-23 19:49:12 +0300

    iOS - bugfix: the app doesn't load the first page

--- src/StatisticsRomania/StatisticsRomania/Views/CountyDetailsView.cs
+++ src/StatisticsRomania/StatisticsRomania/Views/CountyDetailsView.cs
@@ -9,6 +9,7 @@ using StatisticsRomania.Helpers;
 using StatisticsRomania.ViewModels;
 using System;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration;
@@ -81,14 +82,14 @@ namespace StatisticsRomania.Views
             _degChapterData.IsReadOnly = true;
             _degChapterData.HorizontalOptions = LayoutOptions.FillAndExpand;
             _degChapterData.VerticalOptions = LayoutOptions.FillAndExpand;
-            _degChapterData.Columns.Add(new TextColumn() { Caption = "An", FieldName = "Year", IsReadOnly = true, AllowSort = DefaultBoolean.False});
+            _degChapterData.Columns.Add(new TextColumn() { Caption = "An", FieldName = "Year", IsReadOnly = true, AllowSort = DefaultBoolean.False });
             _degChapterData.Columns.Add(new TextColumn() { Caption = "Luna", FieldName = "YearFraction", IsReadOnly = true, AllowSort = DefaultBoolean.False });
             var valueColumn = new TextColumn()
-                                  {
-                                      FieldName = "Value",
-                                      IsReadOnly = true,
-                                      AllowSort = DefaultBoolean.False
-                                  };
+            {
+                FieldName = "Value",
+                IsReadOnly = true,
+                AllowSort = DefaultBoolean.False
+            };
             valueColumn.SetBinding(TextColumn.CaptionProperty, new Binding("ValueColumnCaption", source: _viewModel));
             _degChapterData.Columns.Add(valueColumn);
             var valueColumn2 = new TextColumn()
@@ -107,7 +108,7 @@ namespace StatisticsRomania.Views
             _plotView.SetBinding(PlotView.IsVisibleProperty, new Binding("HasData", source: _viewModel));
             _plotView.HorizontalOptions = LayoutOptions.FillAndExpand;
             _plotView.VerticalOptions = LayoutOptions.FillAndExpand;
-            _plotView.Model = new PlotModel();
+            _plotView.Model = new ViewResolvingPlotModel();
             var series = new LineSeries();
             series.ItemsSource = _viewModel.ChapterData;
             _plotView.Model.Series.Add(series);
@@ -116,9 +117,9 @@ namespace StatisticsRomania.Views
                 _plotView.BackgroundColor = Color.FromRgb(51, 51, 51);
 
             var btnTest = new Button()
-                              {
-                                  Text = "Test"
-                              };
+            {
+                Text = "Test"
+            };
             btnTest.Clicked += btnTest_Clicked;
 
             var grid = new Grid
@@ -143,9 +144,9 @@ namespace StatisticsRomania.Views
                 Orientation = StackOrientation.Horizontal,
                 Padding = new Thickness(0, 2),
                 Children =
-                {
-                    _pickerCounties, lblCompare, _pickerCounties2//, btnTest
-                }
+            {
+                _pickerCounties, lblCompare, _pickerCounties2//, btnTest
+            }
             };
 
             grid.Children.Add(_firstRowOfHeader, 0, 0);
@@ -279,7 +280,7 @@ namespace StatisticsRomania.Views
             if (_plotView == null)
                 return;
 
-            _plotView.Model = _plotView.Model ?? new PlotModel();
+            _plotView.Model = _plotView.Model ?? new ViewResolvingPlotModel();
             _plotView.Model.Title = "Evolutie indicator";
 
             if (Device.RuntimePlatform == Device.Android)
@@ -345,6 +346,35 @@ namespace StatisticsRomania.Views
         private async void picker_SelectedIndexChanged(object sender, EventArgs e)
         {
             await LoadData();
+        }
+    }
+
+    /// <summary>
+    /// The app crashes on iOS when removing, then adding the OxyPlot view to the content's children collection
+    /// So use this hack to avoid it
+    /// </summary>
+    public class ViewResolvingPlotModel : PlotModel, IPlotModel
+    {
+        private static readonly Type BaseType = typeof(ViewResolvingPlotModel).BaseType;
+        private static readonly MethodInfo BaseAttachMethod = BaseType
+            .GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+            .Where(methodInfo => methodInfo.IsFinal && methodInfo.IsPrivate)
+            .FirstOrDefault(methodInfo => methodInfo.Name.EndsWith(nameof(IPlotModel.AttachPlotView)));
+
+        void IPlotModel.AttachPlotView(IPlotView plotView)
+        {
+            //because of issue https://github.com/oxyplot/oxyplot/issues/497 
+            //only one view can ever be attached to one plotmodel
+            //we have to force detach previous view and then attach new one
+            if (plotView != null && PlotView != null && !Equals(plotView, PlotView))
+            {
+                BaseAttachMethod.Invoke(this, new object[] { null });
+                BaseAttachMethod.Invoke(this, new object[] { plotView });
+            }
+            else
+            {
+                BaseAttachMethod.Invoke(this, new object[] { plotView });
+            }
         }
     }
 }
