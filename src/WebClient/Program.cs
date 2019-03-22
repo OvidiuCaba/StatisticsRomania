75c12289cb6ab620ec075b51dcad2b65e1b16275
Ovidiu Caba <older_ovello@yahoo.com>
2019-03-22 17:13:38 +0200

    Add TODO

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using System.IO;
 
 // TODO: numarul de locuinte autorizate in fiecare luna/semestru (Beniamin Petrovai)
+// Ar merge si o piramida a varstelor implementata. (review Google Play 21.03.2019)
 
 namespace WebClient
 {
