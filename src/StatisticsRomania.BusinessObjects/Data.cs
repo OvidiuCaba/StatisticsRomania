e50c39048c7f2af1a672351e2fe0c8c4fc61aa98
Ovidiu Caba <older_ovello@yahoo.com>
2019-04-27 00:00:07 +0300

    Data - remove SQLite net attributes [the goal is to remove SQLite]

--- src/StatisticsRomania.BusinessObjects/Data.cs
+++ src/StatisticsRomania.BusinessObjects/Data.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//using SQLite.Net.Attributes;
-using SQLiteNetExtensions.Attributes;
-using SQLite;
 
 namespace StatisticsRomania.BusinessObjects
 {
     public abstract class Data
     {
-        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
-        [ForeignKey(typeof(County))]
         public int? CountyId { get; set; }
 
-        [MaxLength(256)]
         public string Subchapter { get; set; }
 
         public int Year { get; set; }
@@ -27,13 +17,10 @@ namespace StatisticsRomania.BusinessObjects
 
         public float Value { get; set; }
 
-        [Ignore]
         public float Value2 { get; set; }
 
-        [Ignore]
         public County County { get; set; }
 
-        [Ignore]
         public DateTime TimeStamp
         {
             get { return new DateTime(Year, YearFraction, 1); }
