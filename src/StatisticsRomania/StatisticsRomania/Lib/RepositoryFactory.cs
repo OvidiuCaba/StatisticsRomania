ad664bb68ee8fd78aaded4115740e1bb16981272
Ovidiu Caba <older_ovello@yahoo.com>
2019-04-26 21:00:34 +0300

    RepositoryFactory - cleanup

--- src/StatisticsRomania/StatisticsRomania/Lib/RepositoryFactory.cs
+++ src/StatisticsRomania/StatisticsRomania/Lib/RepositoryFactory.cs
@@ -7,20 +7,10 @@ namespace StatisticsRomania.Lib
     {
         public static IRepository<T> GetRepository<T>() where T : Data, new()
         {
-            return GetWebClientRepository<T>();
-        }
-
-        private static IRepository<T> GetWebClientRepository<T>() where T : Data, new()
-        {
             return new InMemoryRepository<T>();
         }
 
         public static IRepository<County> GetCountyRepository()
-        {
-            return GetWebClientCountyRepository();
-        }
-
-        private static IRepository<County> GetWebClientCountyRepository()
         {
             return new InMemoryRepository<County>();
         }
