ba58d1d0fc93ae9e28aff02925442d7ee4b026fe
Ovidiu Caba <older_ovello@yahoo.com>
2019-11-01 19:54:36 +0200

    Added an empty components/performers.component into the new ClientApp

--- src/WebClient/ClientApp/src/app/app.module.ts
+++ src/WebClient/ClientApp/src/app/app.module.ts
@@ -10,13 +10,17 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+import { PerformersComponent } from './components/performers.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+        FetchDataComponent,
+
+        PerformersComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -25,7 +29,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+        { path: 'fetch-data', component: FetchDataComponent },
+        { path: 'performers', component: PerformersComponent }
     ])
   ],
   providers: [],
---
+++ src/WebClient/ClientApp/src/app/components/performers.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'performers',
+    templateUrl: './performers.component.html',
+})
+
+export class PerformersComponent {
+}
