d1dcb8849d0876aa8835c3869c7621db85044966
Ovidiu Caba <older_ovello@yahoo.com>
2022-10-16 19:54:00 +0300

    UsesCleartextTraffic is needed for Android 11/API level 30

--- src/StatisticsRomania/StatisticsRomania.Droid/Properties/AssemblyInfo.cs
+++ src/StatisticsRomania/StatisticsRomania.Droid/Properties/AssemblyInfo.cs
@@ -31,3 +31,4 @@ using Android.App;
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: Application(UsesCleartextTraffic = true)]
